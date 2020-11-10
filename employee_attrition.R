61b2902448ca6f26ec4fba19e835ef3c560a61bb
Lewis Playfoot <lp00507@surrey.ac.uk>
2020-11-10 18:14:10 +0000

    LP Ignore

--- employee_attrition.R
+++ employee_attrition.R
@@ -1,49 +1,102 @@
-library(dplyr)
-library(tidyverse)
-library(caret)
+rm(list=ls())
+# Global Environment variables
+# - i.e. available to all functions
+# Good practice to place "constants" in named variables
+# I use UPPERCASE to identify these in my code
 
-# load the dataset
-employee_dataset <- read.csv("employee-attrition.csv")
+DATASET_FILENAME  <- "employee-attrition.csv"          # Name of input dataset file
+OUTPUT_FIELD      <- "Attrition"             # Field name of the output class to predict
 
-#remove columns not need
-# EmployeeCount is 1 for all
-# EmployeeNumber is irrelevant 
-# Standard Hours is 80 for all
-# Over18 is Y for each value
-employee_dataset <- employee_dataset %>%
-  select(-c(EmployeeCount, EmployeeNumber, StandardHours, Over18))
+TYPE_DISCREET     <- "DISCREET"           # field is discreet (numeric)
+TYPE_ORDINAL      <- "ORDINAL"            # field is continuous numeric
+TYPE_SYMBOLIC     <- "SYMBOLIC"           # field is a string
+TYPE_NUMERIC      <- "NUMERIC"            # field is initially a numeric
+TYPE_IGNORE       <- "IGNORE"             # field is not encoded
+DISCREET_BINS     <- 5                    # Number of Discreet Bins Required for 
 
-#just another way of doing the statement above
-#select(employee_dataset, -c(EmployeeCount, EmployeeNumber, StandardHours, Over18))
 
-#NPREPROCESSING_initialFieldType(employee_dataset)
 
-# Attrition <- Yes - 2, No - 1
-# BusinessTravels <- Travel_Rarely - 3, Travel_Frequently - 2, Non-Travel - 1
-# Department <- Sales - 3, R & D - 2, Human Resources - 1
-# EducationField <- Life Sciences - 2, Other - 5, Medical - 4, Marketing - 3, Technical Degree - 6, Human Resources - 1
-#  OHE -> Gender
-# Job Role <- Sales Ex - 8, Res Sc - 7, Lab - 3, Manufa - 5, Healthca - 1, Mana - 4, sales - 9, resea dir - 6, human res - 2
-# Maritial Status <- Single - 3, Married - 2, Divorced -1
-# OHE -> Overtime 
+# Define and then load the libraries used in this project
+# Library from CRAN     Version
+# pacman	               0.5.1
+# outliers	             0.14
+# corrplot	             0.84
+# MASS	                 7.3.53
+# formattable 	         0.2.0.1
+# stats                  4.0.3
+# PerformanceAnalytics   2.0.4
+# Carat         
+MYLIBRARIES<-c("outliers",
+               "corrplot",
+               "MASS",
+               "formattable",
+               "stats",
+               "PerformanceAnalytics",
+               "caret")
 
-# all in one move
-#Code
-newdf <- employee_dataset %>% mutate(across(c(Attrition, BusinessTravel, Department, EducationField, JobRole, MaritalStatus),
-                                            ~as.numeric(as.factor(.))))
 
-#One hot encoding done here. 
-ohe_feats_train = c('Gender', 'OverTime')
-dummies_train <- dummyVars(~  Gender + OverTime, data = newdf)
-df_all_ohe_train <- as.data.frame(predict(dummies_train, newdata = newdf))
-df_all_combined_train <- cbind(newdf[,-c(which(colnames(newdf) %in% ohe_feats_train))],df_all_ohe_train)
 
-# Create Bins and complete dataset before normalisation
-dataBeforeNormalisation <- df_all_combined_train %>% mutate(MonthlyIncome = case_when(MonthlyIncome <= 2500 ~ 1,
-                                                                   MonthlyIncome > 2500   & MonthlyIncome <= 5000 ~ 2,
-                                                                   MonthlyIncome > 5000   & MonthlyIncome <= 7500 ~ 3,
-                                                                   MonthlyIncome > 7500   & MonthlyIncome <= 10000 ~ 4,
-                                                                   MonthlyIncome > 10000   & MonthlyIncome <= 12500 ~ 5,
-                                                                   MonthlyIncome > 12500   & MonthlyIncome <= 15000 ~ 6,
-                                                                   MonthlyIncome > 15000   & MonthlyIncome <= 17500 ~ 7,
-                                                                   MonthlyIncome > 17500   & MonthlyIncome <= 20000 ~ 8)) # end function
+
+
+# ************************************************
+# main() :
+# main entry point to execute analytics
+#
+# INPUT       :   None
+#
+# OUTPUT      :   None
+#
+# Keeps all objects as local to this function
+# ************************************************
+main<-function(){
+  
+  print("Inside main function")
+  
+  print(DATASET_FILENAME)
+  
+  #Load the dataset into a variable named originalDataSet.
+  
+  
+  #Do you have unexpected field names or data? There are some complexities when loading text from different systems,
+  #e.g. Windows PC or Mac, where characters are “encoded” differently. You may want to search google on “UTF-8” if you
+  #have compatibility issues. To solve this, load the CSV file into Excel and then “Save as…”, selecting the FILE FORMAT to
+  #save as “MS-DOS Comma Seperated”. Spare a moment to think about what issues you might face with much larger datasets,
+  #collected on different IT systems, maybe in different counties and what might be involved to get this data into a format that you
+  #can use for ML.
+  #Print statistics of originalDataSet into the viewer.
+  basicStatistics(originalDataSet)
+  
+  
+  field_types<-FieldTypes(originalDataSet)
+
+  #numeric_fields <- names(originalDataSet)[field_types=="NUMERIC"]
+  #print(paste("Numeric Fields =", length(numeric_fields)))
+  #print(numeric_fields)
+  #symbolic_fields <- names(originalDataSet)[field_types=="SYMBOLIC"]
+  #print(paste("SYMBOLIC Fields =", length(symbolic_fields)))
+  #print(symbolic_fields)
+
+  discreetDataset <- NPREPROCESSING_discreetNumeric(originalDataSet,field_types,DISCREET_BINS)
+  
+  discreet_fields <- names(originalDataSet)[discreetDataset=="DISCREET"]
+  print(paste("Discreet Fields =", length(discreet_fields)))
+  print(discreet_fields)
+  print("Leaving main")
+  
+} #endof main()
+
+
+# clears the console area
+cat("\014")
+
+# Loads the libraries
+library(pacman)
+pacman::p_load(char=MYLIBRARIES,install=TRUE,character.only=TRUE)
+
+#Load additional R script files provide for this lab
+source("employee_attrition_functions.R")
+
+set.seed(123)
+
+originalDataSet <- readDataset(DATASET_FILENAME)
+main()
---
+++ employee_attrition_functions.R
@@ -0,0 +1,225 @@
+# To manually set a field type
+# This will store $name=field name, $type=field type
+manualTypes <- data.frame()
+
+# ************************************************
+# readDataset() :
+#
+# Read a CSV file from working directory
+#
+# INPUT: string - csvFilename - CSV filename
+#
+# OUTPUT : data frame - contents of the headed CSV file
+# ************************************************
+readDataset<-function(csvFilename){
+  
+  dataset<-read.csv(csvFilename,encoding="UTF-8",stringsAsFactors = FALSE)
+  
+  # The field names "confuse" some of the library algorithms
+  # As they do not like spaces, punctuation, etc.
+  names(dataset)<-removePunctuation(names(dataset))
+  
+  print(paste("CSV dataset",csvFilename,"has been read. Records=",nrow(dataset)))
+  return(dataset)
+}
+
+
+# ************************************************
+# removePunctuation()
+#
+# INPUT: String - fieldName - name of field
+#
+# OUTPUT : String - name of field with punctuation removed
+# ************************************************
+removePunctuation<-function(fieldName){
+  return(gsub("[[:punct:][:blank:]]+", "", fieldName))
+}
+
+
+
+
+# ************************************************
+# basicStatistics()
+# Output simple dataset field analysis results as a table in "Viewer"
+#
+# REQUIRES: formattable
+#
+# INPUT: data frame    - dataset, full dataset used for train/test
+#                      - Each row is one record, each column in named
+#                      - Values are not scaled or encoded
+#        String - OPTIONAL string which is used in table as a header
+#
+# OUTPUT : none
+#
+# Requires the library: PerformanceAnalytics
+#                       formattable
+# ************************************************
+basicStatistics<-function(dataset,...){
+  
+  params <- list(...)
+  
+  tidyTable<-data.frame(Field=names(dataset),
+                        Catagorical=FALSE,
+                        Symbols=0,
+                        Name=0,
+                        Min=0.0,
+                        Mean=0.0,
+                        Max=0.0,
+                        Skew=0.0,
+                        stringsAsFactors = FALSE)
+  
+  if (length(params)>0){
+    names(tidyTable)[1]<-params[1]
+  }
+  
+  for (i in 1:ncol(dataset)){
+    isFieldAfactor<-!is.numeric(dataset[,i])
+    tidyTable$Catagorical[i]<-isFieldAfactor
+    if (isFieldAfactor){
+      tidyTable$Symbols[i]<-length(unique(dataset[,i]))  #Number of symbols in catagorical
+      #Gets the count of each unique symbol
+      symbolTable<-sapply(unique(dataset[,i]),function(x) length(which(dataset[,i]==x)))
+      majoritySymbolPC<-round((sort(symbolTable,decreasing = TRUE)[1]/nrow(dataset))*100,digits=0)
+      tidyTable$Name[i]<-paste(names(majoritySymbolPC),"(",majoritySymbolPC,"%)",sep="")
+    } else
+    {
+      tidyTable$Max[i]<-round(max(dataset[,i]),2)
+      tidyTable$Mean[i]<-round(mean(dataset[,i]),2)
+      tidyTable$Min[i]<-round(min(dataset[,i]),2)
+      tidyTable$Skew[i]<-round(PerformanceAnalytics::skewness(dataset[,i],method="moment"),2)
+    }
+  }
+  
+  #Sort table so that all numerics are first
+  t<-formattable::formattable(tidyTable[order(tidyTable$Catagorical),],
+                              list(Catagorical = formatter("span",style = x ~ style(color = ifelse(x,"green", "red")),
+                                                           x ~ icontext(ifelse(x, "ok", "remove"), ifelse(x, "Yes", "No"))),
+                                   Symbols = formatter("span",style = x ~ style(color = "black"),x ~ ifelse(x==0,"-",sprintf("%d", x))),
+                                   Min = formatter("span",style = x ~ style(color = "black"), ~ ifelse(Catagorical,"-",format(Min, nsmall=2, big.mark=","))),
+                                   Mean = formatter("span",style = x ~ style(color = "black"),~ ifelse(Catagorical,"-",format(Mean, nsmall=2, big.mark=","))),
+                                   Max = formatter("span",style = x ~ style(color = "black"), ~ ifelse(Catagorical,"-",format(Max, nsmall=2, big.mark=","))),
+                                   Skew = formatter("span",style = x ~ style(color = "black"),~ ifelse(Catagorical,"-",sprintf("%.2f", Skew)))
+                              ))
+  print(t)
+}
+
+
+
+# ************************************************
+# NPREPROCESSING_setInitialFieldType() :
+#
+# Set  each field for NUMERIC or SYNBOLIC
+#
+# INPUT:
+#        String - name - name of the field to manually set
+#        String - type - manual type
+#
+# OUTPUT : None
+# ************************************************
+# ************************************************
+# FieldTypes() :
+#
+# Test each field for NUMERIC or SYNBOLIC
+#
+# INPUT: Data Frame - dataset - data
+#
+# OUTPUT : Vector - Vector of types {NUMERIC, SYMBOLIC}
+# ************************************************
+FieldTypes<-function(dataset){
+  
+  field_types<-vector()
+  for(field in 1:(ncol(dataset))){
+    
+    entry<-which(manualTypes$name==names(dataset)[field])
+    if (length(entry)>0){
+      field_types[field]<-manualTypes$type[entry]
+      next
+    }
+    
+    if (is.numeric(dataset[,field])) {
+      field_types[field]<-TYPE_NUMERIC
+    }
+    else {
+      field_types[field]<-TYPE_SYMBOLIC
+    }
+  }
+  return(field_types)
+}
+
+
+# ************************************************
+# rescaleField() :
+#
+# Rescale an numeric field to be between 0-1.
+#
+# INPUT: Vector to rescale
+#
+# OUTPUT : Vector scaled between 0,1
+# ************************************************
+
+rescaleField<-function(input){
+  return((input-min(input))/(max(input)-min(input)))
+}
+
+
+# ************************************************
+# NPREPROCESSING_discreetNumeric() :
+#
+# Test NUMERIC field if DISCREET or ORDINAL
+#
+# INPUT: data frame      - dataset     - input data
+#        vector strings  - field_types - Types per field, either {NUMERIC, SYMBOLIC}
+#        int             - cutoff      - Number of empty bins needed to determine discreet (1-10)
+#
+# OUTPUT : vector strings - Updated with types per field {DISCREET, ORDINAL}
+# ************************************************
+# Uses histogram
+# Plots histogram for visulisation
+# ************************************************
+NPREPROCESSING_discreetNumeric<-function(dataset,field_types,cutoff){
+  
+  #For every field in our dataset
+  for(field in 1:(ncol(dataset))){
+    
+    #Only for fields that are all numeric
+    if (field_types[field]==TYPE_NUMERIC) {
+      
+      #Scale the whole field (column) to between 0 and 1
+      scaled_column<-rescaleField(dataset[,field])
+      
+      #Generate the "cutoff" points for each of 10 bins
+      #so we will get 0-0.1, 0.1-0.2...0.9-1.0
+      cutpoints<-seq(0,1,length=11)
+      
+      #This creates an empty vector that will hold the counts of ther numbers in the bin range
+      bins<-vector()
+      
+      #Now we count how many numbers fall within the range
+      #length(...) is used to count the numbers that fall within the conditional
+      for (i in 2:11){
+        bins<-append(bins,length(scaled_column[(scaled_column<=cutpoints[i])&(scaled_column>cutpoints[i-1])]))
+      }
+      
+      # the 10 bins will have a % value of the count (i.e. density)
+      bins<-(bins/length(scaled_column))*100.0
+      
+      graphTitle<-"AUTO:"
+      
+      #If the number of bins with less than 1% of the values is greater than the cutoff
+      #then the field is deterimed to be a discreet value
+      
+      if (length(which(bins<1.0))>cutoff)
+        field_types[field]<-TYPE_DISCREET
+      else
+        field_types[field]<-TYPE_ORDINAL
+      
+      barplot(bins, main=paste(graphTitle,field_types[field]),
+                       xlab=names(dataset[field]),
+                       names.arg = 1:10,bty="n")
+      #Bar chart helps visulisation. Type of field is the chart name
+        
+      
+    } #endif numeric types
+  } #endof for
+  return(field_types)
+}
