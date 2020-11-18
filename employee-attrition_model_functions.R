4ffcc31bf7521e31127b307d4dbff888962d520c
Ibz <ayomikun8219@gmail.com>
2020-11-18 15:53:58 +0000

    Add confidence to the rules print

--- employee-attrition_model_functions.R
+++ employee-attrition_model_functions.R
@@ -383,8 +383,8 @@ getTreeRules<-function(tree, print = F){
   }
   
   # Use more descriptive column names and drop undesired columns
-  colnames(rules) <- c("Rule Number", "Rule", "Classification", "Occurences in Dataset")
-  rules <- rules[, 1:4]
+  colnames(rules) <- c("Rule Number", "Rule", "Classification", "Occurences in Dataset", "Confidence")
+  rules <- rules[, 1:5]
   
   if (print) {
     print(formattable::formattable(rules))
